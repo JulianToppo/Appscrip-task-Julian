c6604f89fb4d7f691f5ef872f71fa9e89546df8b
JulianToppo <juliantoppo95@gmail.com>
2024-05-18 09:24:11 +0530

    Footer made responsive

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,29 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "@/styles/Footer.module.css";
 import Image from "next/image";
+import { useIsMobile } from "@/util/screenSizeContext";
 
 const Footer = () => {
+  const isMobile = useIsMobile()
+
+  const [showmetamuse, setshowmetamuse] = useState(false)
+  const onClickMetaMuse=(e)=>{
+    e.preventDefault();
+    setshowmetamuse((showmetamuse)=>{
+      return !showmetamuse;
+    })
+  }
+
+  const [showquickLinks, setshowquickLinks] = useState(false)
+  const onClickQuickLinks=(e)=>{
+    e.preventDefault();
+    setshowquickLinks((showquickLinks)=>{
+      return !showquickLinks;
+    })
+  }
+  
+  const [showFollow, setshowFollow] = useState(false)
+  const onClickshowFollow=(e)=>{
+    e.preventDefault();
+    setshowFollow((showFollow)=>{
+      return !showFollow;
+    })
+  }
+
+  const [showPayments, setshowPayments] = useState(false)
+  const onClickshowPayments=(e)=>{
+    e.preventDefault();
+    setshowPayments((showPayments)=>{
+      return !showPayments;
+    })
+  }
+
   return (
     <div className={style.footer}>
       <div className={style.footerSection}>
         <div className={style.footerSectionContent}>
           {/* Be the first to know  */}
           <div className={style.footerSection_group}>
-            <p className={style.subheading}>Be the first to know</p>
+            <h1 className={style.subheading}>Be the first to know</h1>
             <p>Sign Up for updates</p>
           </div>
-          <div className={style.contactDetails}>
-            <h2 className={style.subheading}>Contact Us</h2>
-            <p>+44 123 456 7894</p>
-            <p>customercare@metamuse.com</p>
-          </div>
-        </div>
-        <div className={style.footerSectionSubscription}>
           <div className={style.getSubsciption}>
             <input placeholder="Enter your email"></input>
             <button className={style.subscriptionButton}>Subsrcibe</button>
           </div>
-
+        </div>
+        <div className={style.footerSectionContact}>
+        
+          <div className={style.contactDetails}>
+            <h2 className={style.subheading}>Contact Us</h2>
+            <span className={style.flex}>
+               <p>+44 123 456 7894</p>
+            <p>customercare@metamuse.com</p>
+            </span>
+           
+          </div>
           <div className={style.contactDetails}>
             <h2 className={style.subheading}>Currency</h2>
             <p className={style.currency}>
@@ -68,10 +106,12 @@ const Footer = () => {
               </svg>
               USD
             </p>
-            <p className={style.paymentDisclaimer}>
+           {
+            isMobile?<></>:<span className={style.paymentDisclaimer}>
               Transactions will be completed in Euros and a currency reference
               is available on hover.
-            </p>
+            </span>
+           } 
           </div>
         </div>
       </div>
@@ -80,8 +120,24 @@ const Footer = () => {
       <div className={style.footerSectionLinks}>
         <div className={style.pageLinks}>
           {/* meta muse */}
-          <p className={style.subheading}>metta muse</p>
-          <ul className={style.pageLinks}>
+          <span className={style.dropdownfooter} onClick={onClickMetaMuse}>
+            <p className={style.subheading}>metta muse</p>
+           {
+            isMobile?(<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M3.4001 7.4248L8.83343 12.8581C9.4751 13.4998 10.5251 13.4998 11.1668 12.8581L16.6001 7.4248" stroke="white" stroke-width="1.25" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>):(<></>)
+           } 
+
+          </span>
+        
+          {isMobile ?( showmetamuse && <ul className={style.pageLinks}>
+            <li>About Us</li>
+            <li>Stories</li>
+            <li>Artisans</li>
+            <li>Boutique</li>
+            <li>Contact US</li>
+            <li>EU Compliance Docs</li>
+          </ul>):(<ul className={style.pageLinks}>
             <li>About Us</li>
             <li>Stories</li>
             <li>Artisans</li>
@@ -89,11 +145,31 @@ const Footer = () => {
             <li>Contact US</li>
             <li>EU Compliance Docs</li>
           </ul>
+
+          ) }
         </div>
         <div className={style.pageLinks}>
           {/* Quick links */}
-          <p className={style.subheading}>Quick Links</p>
-          <ul className={style.pageLinks}>
+
+          <span className={style.dropdownfooter} onClick={onClickQuickLinks}>
+            <p className={style.subheading}>Quick Links</p>
+           {
+            isMobile?(<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M3.4001 7.4248L8.83343 12.8581C9.4751 13.4998 10.5251 13.4998 11.1668 12.8581L16.6001 7.4248" stroke="white" stroke-width="1.25" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>):(<></>)
+           } 
+
+          </span>
+        
+          {isMobile ?( showquickLinks && <ul className={style.pageLinks}>
+            <li>Order & Shipping</li>
+            <li>Join/Login as a Sellar</li>
+            <li>Payment & Pricing</li>
+            <li>Order & Shipping</li>
+            <li>Returns & Refund </li>
+            <li>FAQ</li>
+            <li>Terms & Conditions</li>
+          </ul>):(<ul className={style.pageLinks}>
             <li>Order & Shipping</li>
             <li>Join/Login as a Sellar</li>
             <li>Payment & Pricing</li>
@@ -102,13 +178,26 @@ const Footer = () => {
             <li>FAQ</li>
             <li>Terms & Conditions</li>
           </ul>
+
+          ) }
+
+    
         </div>
-        <div className={style.pageLinks}>
+        <div className={style.footerSocialMedia}>
           {/* Follow us  */}
 
           <div>
-            <p className={style.subheading}>Follow Us</p>
-            <svg
+            <span className={style.footerfollow} onClick={onClickshowFollow}>
+                <p className={style.subheading}>Follow Us</p>
+                {
+            isMobile?(<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M3.4001 7.4248L8.83343 12.8581C9.4751 13.4998 10.5251 13.4998 11.1668 12.8581L16.6001 7.4248" stroke="white" stroke-width="1.25" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>):(<></>)
+           } 
+            </span>
+          
+            {isMobile ?( showFollow &&   <span>
+              <svg
               width="34"
               height="34"
               viewBox="0 0 34 34"
@@ -165,11 +254,101 @@ const Footer = () => {
                 fill="white"
               />
             </svg>
+          </span>):(<span>
+              <svg
+              width="34"
+              height="34"
+              viewBox="0 0 34 34"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                x="1"
+                y="1"
+                width="32"
+                height="32"
+                rx="16"
+                fill="black"
+                stroke="white"
+                stroke-width="1.2"
+              />
+              <g clip-path="url(#clip0_20791_242)">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M13.7017 9.048C12.8503 9.08686 12.2686 9.22171 11.7589 9.42057C11.2331 9.624 10.7874 9.89829 10.3417 10.3429C9.89829 10.7874 9.624 11.2343 9.42057 11.76C9.22171 12.2686 9.08686 12.8503 9.048 13.7017C9.00914 14.5543 9 14.8274 9 17C9 19.1726 9.00914 19.4457 9.048 20.2983C9.08686 21.1497 9.22171 21.7314 9.42057 22.2411C9.624 22.7669 9.89829 23.2126 10.3429 23.6583C10.7874 24.1017 11.2343 24.376 11.76 24.5806C12.2686 24.7783 12.8503 24.912 13.7017 24.952C14.5554 24.9909 14.8274 25 17 25C19.1726 25 19.4457 24.9909 20.2983 24.952C21.1497 24.9131 21.7314 24.7783 22.2411 24.5806C22.7669 24.3749 23.2126 24.1017 23.6583 23.6571C24.1017 23.2126 24.376 22.7657 24.5806 22.24C24.7783 21.7314 24.912 21.1497 24.952 20.2983C24.9909 19.4446 25 19.1726 25 17C25 14.8274 24.9909 14.5543 24.952 13.7017C24.9131 12.8503 24.7783 12.2686 24.5806 11.7589C24.3795 11.2249 24.0644 10.7413 23.6571 10.3417C23.2126 9.89829 22.7657 9.624 22.24 9.42057C21.7314 9.22171 21.1497 9.08686 20.2983 9.048C19.4457 9.00914 19.1726 9 17 9C14.8274 9 14.5543 9.00914 13.7017 9.048ZM12.8914 17C12.8914 18.0897 13.3243 19.1347 14.0948 19.9052C14.8653 20.6757 15.9103 21.1086 17 21.1086C18.0897 21.1086 19.1347 20.6757 19.9052 19.9052C20.6757 19.1347 21.1086 18.0897 21.1086 17C21.1086 15.9103 20.6757 14.8653 19.9052 14.0948C19.1347 13.3243 18.0897 12.8914 17 12.8914C15.9103 12.8914 14.8653 13.3243 14.0948 14.0948C13.3243 14.8653 12.8914 15.9103 12.8914 17ZM17 19.6663C16.2929 19.6663 15.6147 19.3854 15.1147 18.8853C14.6146 18.3853 14.3337 17.7071 14.3337 17C14.3337 16.2929 14.6146 15.6147 15.1147 15.1147C15.6147 14.6146 16.2929 14.3337 17 14.3337C17.7071 14.3337 18.3853 14.6146 18.8853 15.1147C19.3854 15.6147 19.6663 16.2929 19.6663 17C19.6663 17.7071 19.3854 18.3853 18.8853 18.8853C18.3853 19.3854 17.7071 19.6663 17 19.6663ZM22.6 12.44C22.6 12.5661 22.5752 12.6909 22.5269 12.8074C22.4787 12.9238 22.408 13.0297 22.3188 13.1188C22.2297 13.208 22.1238 13.2787 22.0074 13.3269C21.8909 13.3752 21.7661 13.4 21.64 13.4C21.5139 13.4 21.3891 13.3752 21.2726 13.3269C21.1562 13.2787 21.0503 13.208 20.9612 13.1188C20.872 13.0297 20.8013 12.9238 20.7531 12.8074C20.7048 12.6909 20.68 12.5661 20.68 12.44C20.68 12.1854 20.7811 11.9412 20.9612 11.7612C21.1412 11.5811 21.3854 11.48 21.64 11.48C21.8946 11.48 22.1388 11.5811 22.3188 11.7612C22.4989 11.9412 22.6 12.1854 22.6 12.44Z"
+                  fill="white"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_20791_242">
+                  <rect
+                    width="32"
+                    height="32"
+                    fill="white"
+                    transform="translate(1 1)"
+                  />
+                </clipPath>
+              </defs>
+            </svg>
+            <svg
+              width="34"
+              height="34"
+              viewBox="0 0 34 34"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                x="0.999756"
+                y="1"
+                width="32"
+                height="32"
+                rx="16"
+                stroke="white"
+                stroke-width="1.2"
+              />
+              <path
+                d="M24 8C24.5304 8 25.0391 8.21071 25.4142 8.58579C25.7893 8.96086 26 9.46957 26 10V24C26 24.5304 25.7893 25.0391 25.4142 25.4142C25.0391 25.7893 24.5304 26 24 26H10C9.46957 26 8.96086 25.7893 8.58579 25.4142C8.21071 25.0391 8 24.5304 8 24V10C8 9.46957 8.21071 8.96086 8.58579 8.58579C8.96086 8.21071 9.46957 8 10 8H24ZM23.5 23.5V18.2C23.5 17.3354 23.1565 16.5062 22.5452 15.8948C21.9338 15.2835 21.1046 14.94 20.24 14.94C19.39 14.94 18.4 15.46 17.92 16.24V15.13H15.13V23.5H17.92V18.57C17.92 17.8 18.54 17.17 19.31 17.17C19.6813 17.17 20.0374 17.3175 20.2999 17.5801C20.5625 17.8426 20.71 18.1987 20.71 18.57V23.5H23.5ZM11.88 13.56C12.3256 13.56 12.7529 13.383 13.0679 13.0679C13.383 12.7529 13.56 12.3256 13.56 11.88C13.56 10.95 12.81 10.19 11.88 10.19C11.4318 10.19 11.0019 10.3681 10.685 10.685C10.3681 11.0019 10.19 11.4318 10.19 11.88C10.19 12.81 10.95 13.56 11.88 13.56ZM13.27 23.5V15.13H10.5V23.5H13.27Z"
+                fill="white"
+              />
+            </svg>
+          </span>
+
+          ) }
+
+
+    
+          
           </div>
 
           <div>
-            <p>meta musse accepts</p>
-            <div>
+
+            <span className={style.footerfollow} onClick={onClickshowPayments}>
+               <p className={style.subheading}>meta musse accepts</p>
+               {
+            isMobile?(<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M3.4001 7.4248L8.83343 12.8581C9.4751 13.4998 10.5251 13.4998 11.1668 12.8581L16.6001 7.4248" stroke="white" stroke-width="1.25" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>):(<></>)
+           } 
+            </span>
+           
+
+            {isMobile ?( showPayments &&     <div>
+              <Image src={"/paymenticons/gpay.png"} width={30} height={20} />
+              <Image src={"/paymenticons/visa.png"} width={30} height={20} />
+              <Image src={"/paymenticons/paypal.png"} width={30} height={20} />
+              <Image
+                src={"/paymenticons/applepay.png"}
+                width={30}
+                height={20}
+              />
+              <Image
+                src={"/paymenticons/applepay.png"}
+                width={30}
+                height={20}
+              />
+              <Image src={"/paymenticons/pay.png"} width={30} height={20} />
+            </div>):(    <div>
               <Image src={"/paymenticons/gpay.png"} width={30} height={20} />
               <Image src={"/paymenticons/visa.png"} width={30} height={20} />
               <Image src={"/paymenticons/paypal.png"} width={30} height={20} />
@@ -185,6 +364,10 @@ const Footer = () => {
               />
               <Image src={"/paymenticons/pay.png"} width={30} height={20} />
             </div>
+
+          ) }
+           
+
           </div>
         </div>
       </div>
