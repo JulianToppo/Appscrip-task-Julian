871814110c51ca8d08eb3f56d45bfac41529d5ff
JulianToppo <juliantoppo95@gmail.com>
2024-05-18 07:06:28 +0530

    Header made responsive using screen size context

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import {MobileProvider} from "@/util/screenSizeContext.js";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MobileProvider>
+      <Component {...pageProps} />;
+    </MobileProvider>
+  );
 }
