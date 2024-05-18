5b3ac9dd73b6651045c33231663fe563d5bf6405
JulianToppo <juliantoppo95@gmail.com>
2024-05-18 10:09:24 +0530

    Addition of head

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,16 @@ import Header from "@/components/Header/Header";
 import PageHeading from "@/components/PageHeading/PageHeading";
 import Products from "@/components/Products/Products";
 import React from "react";
+import Head from "next/head";
 
 export async function getStaticProps() {
   const res = await fetch("https://fakestoreapi.com/products");
   const products = await res.json();
 
-
-  const categoriesResult = await fetch("https://fakestoreapi.com/products/categories");
+  const categoriesResult = await fetch(
+    "https://fakestoreapi.com/products/categories"
+  );
   const categories = await categoriesResult.json();
-
-
 
   return {
     props: {
@@ -22,14 +22,17 @@ export async function getStaticProps() {
   };
 }
 
-const Home = ({ products ,categories}) => {
+const Home = ({ products, categories }) => {
   return (
     <div>
-      
+      <Head>
+        <title>Appscrip Interview Task</title>
+        <meta name="description" content="This is a part of appscrip interview process which clones an ecommerce website" />
+      </Head>
       <Header />
       <PageHeading />
-      <Products products={products} categories={categories}/>
-      <Footer/>
+      <Products products={products} categories={categories} />
+      <Footer />
     </div>
   );
 };
